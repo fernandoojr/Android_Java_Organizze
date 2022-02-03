a614da5bb79d436ead6314ff5fcfd9648d1cf6bb
fernandofojr <fernandofojr@gmail.com>
2022-02-03 19:05:09 -0300

    Criadas telas de adicionar despesas e receitas.

--- app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
@@ -1,5 +1,6 @@
 package com.cursoandroid.organizze.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -34,10 +35,10 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
     public void adicionarDespesa(View view){
-
+        startActivity(new Intent(this, DespesasActivity.class));
     }
 
     public void adicionarReceita(View view){
-
+        startActivity(new Intent(this, ReceitasActivity.class));
     }
 }
